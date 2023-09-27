98023eeb56f11a1061bb4f87629cfca497ca2715
Ruan Pinheiro Lima <ruanpinheirolima2003@gmail.com>
2023-09-27 10:53:38 -0300

    Update desafio19

---
+++ desafio19/script.js
@@ -0,0 +1,8 @@
+// let button = document.getElementById('btn')
+// let nav = document.getElementById('navegation')
+
+// function toggleMenu(){
+//     nav.classList.toggle('active')
+// }
+
+// addEventListener('click', toggleMenu())
