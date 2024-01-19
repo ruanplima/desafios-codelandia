a824c57ebd4cd26d74978a7049c27c021cdfa824
Ruan Pinheiro Lima <ruanpinheirolima2003@gmail.com>
2024-01-19 16:50:09 -0300

    Create projeto eFront

--- desafio04/script.js
+++ desafio04/script.js
@@ -1,7 +1,6 @@
-let $password = document.getElementById('senha')
-let $image = document.getElementById('imagem')
-
 function OpenClose(){
+    let $password = document.getElementById('senha')
+    let $image = document.getElementById('imagem')
     if($password.type == 'password'){
         $password.setAttribute('type' , 'text')
         $image.setAttribute('src' , 'images-password/img-close.png')
@@ -10,3 +9,5 @@ function OpenClose(){
         $image.setAttribute('src' , 'images-password/img-open.png')
     }
 }
+
+
