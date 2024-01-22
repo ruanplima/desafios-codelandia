308c99104b21bc79622feda5bd78b5303ca17c0e
Ruan Pinheiro Lima <ruanpinheirolima2003@gmail.com>
2024-01-22 17:34:28 -0300

    Add dark/light mode

---
+++ projeto-eFront/script.js
@@ -0,0 +1,12 @@
+const body = document.querySelector('body')
+const btn = document.querySelector('button.mode')
+const icon = document.querySelector('button.mode i')
+
+function darkMode(){
+    body.classList.toggle('light')
+    if(icon.classList.contains('fa-sun')){
+        icon.classList.toggle('fa-moon')
+    }
+}
+
+btn.addEventListener('click', darkMode)
